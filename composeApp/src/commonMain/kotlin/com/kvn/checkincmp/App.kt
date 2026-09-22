45d29f56e8248c5c4200ea2526d54f95a09b3523
kavinrajan <kavinece53@gmail.com>
2026-09-23 01:26:56 +0530

    Login page loder and screen launch fix

--- composeApp/src/commonMain/kotlin/com/kvn/checkincmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/kvn/checkincmp/App.kt
@@ -1,9 +1,13 @@
 package com.kvn.checkincmp
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalInspectionMode
 import com.kvn.checkincmp.di.appModule
 import com.kvn.checkincmp.navigation.TrevnorNavRoot
@@ -35,9 +39,9 @@ private fun AppContent(viewModel: AppViewModel) {
         if (!uiSource.value.isLoading) {
             TrevnorNavRoot(uiSource.value.authToken)
         } else {
-            androidx.compose.foundation.layout.Box(
-                modifier = androidx.compose.ui.Modifier.fillMaxSize(),
-                contentAlignment = androidx.compose.ui.Alignment.Center
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
                 CircularProgressIndicator()
             }
